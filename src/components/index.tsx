1e4c042385eb6429fbcfd7de74d8ffba788d1406
wangbiao <wangb8909@gmail.com>
2021-06-16 17:33:37 +0800

    feat: remove useless component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,3 @@ export { default as MenuFoldButton } from './buttons/menu-fold-button';
 export { default as Exception403 } from './exception/403';
 export { default as Exception404 } from './exception/404';
 export { default as Exception500 } from './exception/500';
-export { default as LdapUserSelect } from './selector/ldap-user-select';
